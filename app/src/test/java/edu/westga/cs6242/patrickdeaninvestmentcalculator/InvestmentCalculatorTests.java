bb648ea065a138bf1f641d240e5394d91227a43c
pdean1 <pdean1@my.westga.edu>
2016-03-09 10:55:52 -0500

    Added tests to test to make sure exceptions are thrown at correct times.

--- app/src/test/java/edu/westga/cs6242/patrickdeaninvestmentcalculator/InvestmentCalculatorTests.java
+++ app/src/test/java/edu/westga/cs6242/patrickdeaninvestmentcalculator/InvestmentCalculatorTests.java
@@ -1,10 +1,11 @@
 package edu.westga.cs6242.patrickdeaninvestmentcalculator;
 
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import edu.westga.cs6242.patrickdeaninvestmentcalculator.model.InvestmentCalculator;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
@@ -31,5 +32,17 @@ public class InvestmentCalculatorTests {
     public void investmentCalculator_CalculateIsCorrectWithNormalNumbers() throws Exception {
         InvestmentCalculator i = new InvestmentCalculator(300, .05, 36);
         assertEquals(28750.89, i.calculateFutureValueOfAnAnnuity(), 0.1);
+    }
+
+    /**
+     * Testing for exceptions
+     */
+    public final ExpectedException exception = ExpectedException.none();
+    @Test
+    public void investmentCalculator_CalculateThrowsException() throws Exception {
+        exception.expect(Exception.class);
+        InvestmentCalculator i = new InvestmentCalculator(0, .05, 36);
+        i.calculateFutureValueOfAnAnnuity();
+
     }
 }
